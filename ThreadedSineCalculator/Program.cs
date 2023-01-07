1d23b4929ea45fd7a140bdfe634afce17421ad1a
Oleg <oleg.dragoi228@gmail.com>
2023-01-07 17:11:45 +0200

    Created a database connection

--- ThreadedSineCalculator/Program.cs
+++ ThreadedSineCalculator/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace ThreadedSineCalculator
 {
@@ -10,6 +12,11 @@ namespace ThreadedSineCalculator
     {
         static void Main(string[] args)
         {
+            string connStr = "server=localhost;user=admin;database=sine_database;port=3306;password=admin";
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+
+            Console.ReadKey();
         }
     }
 }
